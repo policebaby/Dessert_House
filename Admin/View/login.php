144e83ba91fa795976c97894b3dca2a849fbcbfd
Beyond <policebaby111@gmail.com>
2023-03-13 00:54:46 +0630

    2023/03/13 00:53 Yan Aung Khaing 1.Finish Admin Log in Frontend 2.Add root css for Admin

--- Admin/View/login.php
+++ Admin/View/login.php
@@ -1,4 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link with root css and log in css -->
+    <link rel="stylesheet" href="./resources/css/root.css">
+    <link rel="stylesheet" href="./resources/css/logIn.css">
+
+    <!-- Import google fonts: public display & dm sen serif -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Public+Sans&display=swap" rel="stylesheet">
+
+    <title>Admin Log In</title>
+</head>
+<body>
+    <div class="firstContainer">
+        <img src="./resources/img/logo.png" alt="Fail to load Logo" class="logo">
+
+        <div class="upperContainer">
+
+        </div>
+        
+        <div class="lowerContainer">
+        <div class="title">DESSERT HOUSE</div>
+        <div class="loginText">Log in</div>
+        <input type="text" placeholder="Admin Name" id="adminName" class="nameInputBox">
+        <input type="text" placeholder="Password" id="adminpassword" class="passwordInputBox">
+        <button class="logInButton">Log in</button>
+        </div>
+    </div>
 
 
-?>
+
+
+
+    <!-- background circles -->
+    <div class="upperLeftOuterCircle"></div>
+    <div class="upperLeftInnerCircle"></div>
+    <div class="lowerRightOuterCircle"></div>
+    <div class="lowerRightInnerCircle"></div>    
+</body>
+</html>
