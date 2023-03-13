7990e244e3866e4be678efa451f306803bd997f1
thethmuebgon <thethmuebgon37@gmail.com>
2023-03-13 21:32:15 +0630

    footer

---
+++ Customer/View/footer.php
@@ -0,0 +1,143 @@
+<?php
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css link start -->
+    <link rel="stylesheet" href="../View/resources/css/footer.css" />
+    <link rel="stylesheet" href="../View/resources/css/root.css" />
+    <!-- css link end -->
+    <!-- font family start -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@1&display=swap"
+      rel="stylesheet"
+    />
+    <!-- font family end -->
+    <!-- bootstrap start -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+    <!-- bootstrap end -->
+<!-- js link start -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <!-- js link end -->
+    <title>footer</title>
+</head>
+<body>
+    <div class="row footer container-fluid ">
+        <div class="col-md-6">
+        <div class="mt-3 ms-4">
+        <div class="d-flex">
+          <img src="../image/logo.png" alt="logo" width="25%" class="mt-4" />
+          <h4 class="logoName fs-6 mt-4 pt-3 text-white ms-2">Dessert House</h4>
+        </div>
+        <br />
+        <h6 class="text-white fs-5">Subscribe our website</h6>
+        <div class="mailbox">
+          <iconify-icon icon="logos:google-gmail"></iconify-icon>
+          <input
+            type="text"
+            class="mailText text-white-50"
+            placeholder="Enter Your Email"
+          />
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <h4 class="subHeader text-white ">Need Help?</h4>
+        <br />
+        <p class="text-white">
+          Contact Admin <br />
+          Contact Seller <br />
+          Guide <br />
+          About us <br />
+          Privacy Policy
+        </p>
+      </div>
+    </div>
+
+
+        <div class="col-md-6">
+        <div class="mt-4 me-5">
+        <h4 class="subHeader text-white">Follow us</h4>
+        <div class="mt-2 mb-2 ">
+          <iconify-icon
+            icon="logos:facebook"
+            style="color: #699bf7"
+            width="2em"
+            height="2em"
+          ></iconify-icon>
+          <iconify-icon
+            icon="skill-icons:instagram"
+            style="color: #699bf7"
+            width="2em"
+            height="2em"
+            class="mx-2"
+          ></iconify-icon>
+          <iconify-icon
+            icon="bi:twitter"
+            style="color: #699bf7"
+            width="2em"
+            height="2em"
+          ></iconify-icon>
+        </div>
+        <h4 class=" subHeader text-white ">Address</h4>
+        <p class="text-white normalText">
+          No.66, Bahan Township, Yangon, <br />
+          Myanmar. <br />
+          +95 977777777<br />
+          desserthouse@gmail.com
+        </p>
+      </div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
