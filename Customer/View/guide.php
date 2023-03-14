e80349c8efafaea6614e687d85796db0b7d39578
lwin_Oo <jumj5347@gmail.com>
2023-03-14 15:53:33 +0630

    guide page (FE) 90%

--- Customer/View/guide.php
+++ Customer/View/guide.php
@@ -58,7 +58,25 @@
     <!-- nav end -->
     <!-- FAQ Start -->
     <div class="container">
-        <p class="header text-center">Frequently asked Questions </p>
+        <p class="FAQheader text-center ">Frequently asked Questions </p>
+        <ul style="list-style-type:disc" class="FAQbox">
+            <li>
+                <p class="FAQ">What is <span class="logoName"> DESSERT HOUSE ?</span></p>
+                <p><span class="logoName"> DESSERT HOUSE</span> is a website that will help customers to be convenience and reduced the time of waiting before enjoying desserts at our food court.</p>
+            </li>
+            <li>
+                <p class="FAQ">What is coin ?</p>
+                <p> Coin is the currency that will be used in our system to buy the project of our website and the purpose is to make customer experienced easier times while using our website.</p>
+            </li>
+            <li>
+                <p class="FAQ">What is booking system ?</p>
+                <p> Booking system is for customers who want to make sure the seat are available before coming to our food courts and to avoid cases where customers have to experience queuing for a long time.</p>
+            </li>
+            <li>
+                <p class="FAQ">Is it possible to pay with cash without buying coins?</p>
+                <p>We prioritized the customer’s satisfactory the most. Which is why we accept both the coin payment, cash payment as well as credit cards.</p>
+            </li>
+        </ul>
     </div>
     <!-- FAQ end -->
     <!-- footer start -->
