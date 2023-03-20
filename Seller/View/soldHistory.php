68e1f140a7a2bdabaf253a03e3333b6fc7ca4986
DESKTOP-NPJLU8B\User <thaetun99@gmail.com>
2023-03-20 15:36:21 +0630

    Thae Tun--edit--sold history

--- Seller/View/soldHistory.php
+++ Seller/View/soldHistory.php
@@ -17,71 +17,63 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@1&family=Public+Sans&display=swap" rel="stylesheet">
+    <!-- google icon link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <!-- ionic icon link -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <title>Sold History Page</title>
 </head>
 <body class="sold_history">
-    <div class="main-page">
-        <div class="row">
-            <nav class="col-2 mt-0">
-                <div class="me-0 ms-0 edit_nav">
-                    <div class="nav_logo">
-                        <img src="./resources/images/Ellipse 3.png" alt="logo" width="35%" class="web_logo mt-md-5 mt-sm-5">
-                        <p class="web_name mb-4 mt-3">DESSERT HOUSE</p>
-                    </div>
-                    <div class="ms-md-4">
-                        <!-- profile icon from left nav -->
-                        <div class="nav_text text-muted mt-4">
-                            <img src="./resources/images/person.png" class="icons">
-                            <span class="title ms-1">Profile</span>
-                        </div>
-                        <!-- meun icon from left nav -->
-                        <div class="nav_text my-md-1 text-muted">
-                            <img src="./resources/images/menu.png" class="my-md-5 icons">
-                            <span class="title ms-1">Product Menu</span>
-                        </div>
-                        <!-- order list icon from left nav -->
-                        <div class="nav_text text-muted">
-                            <img src="./resources/images/orderList.png" class="icons">
-                            <span class="title ms-1">Order Lists</span>
-                        </div>
-                        <!-- sold history icon from left nav -->
-                        <div class="nav_text active">
-                            <img src="./resources/images/soldList.png"class="my-md-5 soldHistory-logo">
-                            <span class="title ms-1">Sold History</span>
-                        </div>
-                        <!-- incoming order icon from left nav -->
-                        <div class="nav_text text-muted">
-                            <img src="./resources/images/incomingOrder.png" class="icons">
-                            <span class="title ms-1">Incoming Orders</span>
-                        </div>
-                        <!-- feedback icon -->
-                        <div class="nav_text mt-md-5 text-muted feedback_gp">
-                            <img src="./resources/images/feedback.png" class="icons cFeedback mt-1" width="13%" height="5%" >
-                            <p class="title feedback_text ms-1">Customer's Feedback</p>
-                        </div>
-                        <!-- logout icon from left nav-->
-                        <div class="nav_text text-muted my-md-5">
-                            <img src="./resources/images/Logout.png" alt="" class="nav_text icons logout">
-                            <span class="title ms-1">Logout</span>
-                        </div><br>
-                    </div><br>
-                </nav>
-
-                <!-- for page -->
-                <div class="col-10">
-                <p class="h4 my-md-4 my-sm-4 text-center fw-bold sold-title">Sold History</p>
-                <div class="blue-width me-4">
-        <table class="table table-borderless ms-md-2">
+    <nav class="side-nav">
+        <div class="mt-4">
+            <img src="./resources/images/Ellipse 3.png" alt="webLogo" class="web-logo ms-5">
+            <p class="web-name ms-4">DESSERT HOUSE</p>
+        </div>
+        <div class="side">
+            <ion-icon name="person-circle-outline" class="profile-icon"></ion-icon>
+        <!-- <span class="material-symbols-outlined profile-icon">account_circle</span> -->
+        <span class="profile-names ms-2 mt-2">Profile</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">menu_book</span>
+        <span class="nav-names ms-2 mt-2">Product Menu</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">order_approve</span>
+        <span class="nav-names ms-2 mt-2">Order List</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">pending_actions</span>
+        <span class="nav-names ms-2 mt-2">Sold History</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">order_play</span>
+        <span class="nav-names ms-2 mt-2">Incoming Order</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">sms_failed</span>
+        <span class="nav-names ms-2 mt-2">Customer's Feedback</span>
+        </div>
+        <div class="side">
+        <span class="material-symbols-outlined nav-icons">logout</span>
+        <span class="nav-names ms-2 mt-2">Logout</span>
+        </div>
+    </nav>
+    
+    <div class="page row ms-4">
+        <p class="h4 fw-bold my-md-4 my-sm-4 text-center">Sold History</p>
+        <div class="blue-width ms-md-4">
+            <table class="table table-borderless ms-md-4 ms-sm-3 w-100">
             <tr class="t-head">
             <td>No.</td>
             <td>Date</td>
             <td>Order No.</td>
-            <td class="col-md-2">Items</td>
-            <td>Total Price</td>
-            <td class="title-none">Reservation Time</td>
+            <td class="col-md-2 col-sm-2">Items</td>
+            <td class="title-name">Total Price</td>
+            <td class="title-name">Reservation Time</td>
             </tr>
             <tr>
                 <td class="td-text fw-bold">1</td>
@@ -93,36 +85,19 @@
                     <ion-icon name="chevron-down-outline" class="down" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></ion-icon>
                     </span>
                 </td>
-                <td class="td-text fw-bold">$ 80</td>
-                <td class="td-text fw-bold title-none">2023/03/06 14:30:00</td>
+                <td class="td-text fw-bold title-name">$ 80</td>
+                <td class="td-text fw-bold title-name">2023/03/06 14:30:00</td>
             </tr>
             <tr class="color-tr">
                 <td class=" td-text fw-bold">2</td>
                 <td class=" td-text fw-bold">2023/03/05</td>
                 <td class=" td-text fw-bold">DS_030001</td>
-                <td class=" td-text fw-bold types">
-                <span>Americano coffee x  2</span>
-                    <span>
-                    <ion-icon name="chevron-down-outline" class="down" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></ion-icon>
-                    </span>
-                </td>
-                <td class=" td-text fw-bold">$12</td>
-                <td class=" td-text fw-bold title-none">2023/03/05 15:30:00</td>
-            </tr>
-            <tr>
-                <td>3</td>
-            </tr>
-            <tr class="color-tr">
-                <td>4</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class=" td-text fw-bold">Americano coffee x 2</td>
+                <td class=" td-text fw-bold title-name">$12</td>
+                <td class=" td-text fw-bold title-name">2023/03/05 15:30:00</td>
             </tr>
             </table>
-
-            <!-- popup screen -->
+            <!-- popup screen-->
             <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
@@ -142,16 +117,16 @@
                     <div class="mb-4">
                         <hr>
                         <span class="ms-3">Total</span>
-                        <span class="me-5 total_items">$80</span>
+                        <span class="total_items me-5">$80</span>
                     </div>
                 </div>
             </div>
         </div>
-                </div>
+        </div>
 
-                <!-- for pagination -->
+        <!-- for pagination -->
         <nav aria-label="">
-                <ul class="pagination">
+                <ul class="pagination mb-sm-5">
                     <li class="page-item disabled">
                         <a class="page-link great" href="#" tabindex="-1" aria-disabled="true">
                             <ion-icon name="chevron-back-outline"></ion-icon>
@@ -174,6 +149,7 @@
                 </ul>
             </nav>
         </div>
+
     </div>
     </body>
 </html>
