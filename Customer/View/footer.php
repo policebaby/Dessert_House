f3abe66c2a72a24e9ca5b1011e34b1846fca5cec
lwin_Oo <jumj5347@gmail.com>
2023-03-20 08:32:56 +0630

    footer bootstrap link comment open

--- Customer/View/footer.php
+++ Customer/View/footer.php
@@ -19,7 +19,7 @@
   <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@1&display=swap" rel="stylesheet" />
   <!-- font link end -->
         <!-- bootstrap link start -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" /> -->
     <!-- bootstrap link end -->
   
   <!-- icon link start -->
